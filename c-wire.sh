7b13b1deed81e9e3cab853abfd88fd1fafa35e03
hibamesbahi <hibamesbahi2023@gmail.com>
2024-12-09 13:47:16 +0100

    shell modification

--- c-wire.sh
+++ c-wire.sh
@@ -20,3 +20,55 @@ if [ ! -x "$FILE_C" ]; then
 fi
 
 ./code
+
+if [[ "$1" == "hvb" ]] && [[ "$2" == "comp" ]]; then
+  awk -F';' '$2 != "-" && $4 == "-" && $7 == "-" || $2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"' "cwire.dat" | cut -d';' -f2,5,7,8 | tr '-' '0' | ./projet
+fi
+
+if [[ "$1" == "hvb" ]] && [[ "$2" == "indiv" ]]; then
+  echo "Erreur : les stations HV-B ne peuvent pas avoir de particuliers comme consommateurs."
+  ./help.sh
+fi
+
+if [[ "$1" == "hvb" ]] && [[ "$2" == "all" ]]; then
+  echo "Erreur : les stations HV-B ne peuvent pas avoir tous les types de consommateurs."
+  ./help.sh
+fi
+
+if [[ "$1" == "hva" ]] && [[ "$2" == "comp" ]]; then
+  awk -F';' '$2 != "-" && $4 == "-" && $7 == "-" || $2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"' "cwire.dat" | cut -d';' -f2,5,7,8 | tr '-' '0' | ./projet
+fi
+
+if [[ "$1" == "hva" ]] && [[ "$2" == "indiv" ]]; then
+  echo "Erreur : les stations HV-A ne peuvent pas avoir de particuliers comme consommateurs."
+  ./help.sh
+fi
+
+if [[ "$1" == "hva" ]] && [[ "$2" == "all" ]]; then
+  echo "Erreur : les stations HV-A ne peuvent pas avoir tous les types de consommateurs."
+  ./help.sh
+fi
+
+if [[ "$1" == "lv" ]] && [[ "$2" == "comp" ]]; then
+  awk -F';' '$2 != "-" && $4 == "-" && $7 == "-" || $2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"' "cwire.dat" | cut -d';' -f2,5,7,8 | tr '-' '0' | ./projet
+fi
+
+if [[ "$1" == "lv" ]] && [[ "$2" == "indiv" ]]; then
+  awk -F';' '$2 != "-" && $4 == "-" && $7 == "-" || $2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"' "cwire.dat" | cut -d';' -f2,5,7,8 | tr '-' '0' | ./projet
+fi
+
+if [[ "$1" == "lv" ]] && [[ "$2" == "all" ]]; then
+  awk -F';' '$2 != "-" && $4 == "-" && $7 == "-" || $2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"' "cwire.dat" | cut -d';' -f2,5,7,8 | tr '-' '0' | ./projet
+fi
+
+if [[ "$1" == "-h" ]]; then
+  ./help.sh
+  exit 0
+fi
+
+# Vérification des options
+if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
+  echo "Erreur : il manque des paramètres."
+  ./help.sh
+  exit 1
+fi
