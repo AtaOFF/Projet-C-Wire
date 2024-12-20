ac78b5231b831df39233a9d0e43a39d5f1e8f892
Aliciakl <kellaialic@cy-tech.fr>
2024-12-20 17:41:06 +0100

    corrections C

--- codeC/src/main.c
+++ codeC/src/main.c
@@ -2,27 +2,27 @@
 
 
 int main() {
-    Station* tree=NULL;
-    Station* node=NULL;
-    int ret=0;
-    int v1;
-    long v2,v3; 
+    Station* tree = NULL;
+    Station* node = NULL;
+    int return = 0;
+    int arg1;
+    long arg2,arg3; 
     int h;
     printf("Identifier;Capacity;Load\n");
     do{
-        ret=scanf("%d;%ld;%ld\n",&v1,&v2,&v3);
-        if(ret==3 && search(tree,v1,&node)==0){
-            tree=insertStation(tree,v1,v2,v3,&h); 
+        return = scanf("%d;%ld;%ld\n", &arg1, &arg2, &arg3);
+        if(return == 3 && search(tree, arg1, &node) == 0){
+            tree = insertStation(tree, arg1, arg2, arg3, &h); 
         }
        
-        else if(ret==3 && search(tree,v1,&node)==1 && v2!=0){
-            node->capacity=v2;
+        else if(return == 3 && search(tree, arg1, &node) == 1 && arg2 != 0){
+            node -> capacity = arg2;
         }
-        else if(ret==3 && search(tree,v1,&node)==1 && v3!=0){
-            node->load+=v3;
+        else if(return == 3 && search(tree, arg1, &node) == 1 && v3 != 0){
+            node -> load += arg3;
         }    
 
-    } while(ret==3);
+    } while(return == 3);
     Infix(tree);
     return 0;
 }
